d34554ca778ca0dc511d5d696f0a52bf26a0eb65
DESKTOP-DJK1QF7\akito <akitochu@gmail.com>
2023-05-03 10:05:54 +0100

    completed lambda lab

--- LambdaLab_StarterCode/LambdaLab_Lib/Exercises.cs
+++ LambdaLab_StarterCode/LambdaLab_Lib/Exercises.cs
@@ -1,47 +1,48 @@
-﻿namespace LambdaLab_Lib
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace LambdaLab_Lib
 {
     public class Exercsises
     {
         public static int CountTotalNumberOfSpartans(List<Spartan> spartans)
         {
-            throw new NotImplementedException();
+            return spartans.Count();
         }
 
         public static int CountTotalNumberOfSpartansInUKAndUSA(List<Spartan> spartans)
         {
-            throw new NotImplementedException();
+            return spartans.Count(x => x.Country == "U.K." || x.Country == "U.S.A.");
         }
 
         public static int NumberOfSpartansBornAfter1980(List<Spartan> spartans)
         {
-            throw new NotImplementedException();
+            return spartans.Count(x => x.DateOfBirth.Year > 1980);
         }
 
         public static int SumOfAllSpartaMarksMoreThan50Inclusive(List<Spartan> spartans)
         {
-            throw new NotImplementedException();
+            return spartans.Sum(x => x.SpartaMark >= 50 ? x.SpartaMark : 0);
         }
 
         //To 2 decimal points
         public static double AverageSpartanMark(List<Spartan> spartans)
         {
-            throw new NotImplementedException();
+            return Math.Round(spartans.Average(x => x.SpartaMark), 2);
         }
 
         public static List<Spartan> SortByAlphabeticallyByLastName(List<Spartan> spartans)
         {
-            throw new NotImplementedException();
-
+            return spartans.OrderBy(x => x.LastName).ToList();
         }
 
         public static List<string> ListAllDistinctCities(List<Spartan> spartans)
         {
-            throw new NotImplementedException();
+            return spartans.Select(x => x.City).Distinct().ToList();
         }
 
         public static List<Spartan> ListAllSpartansWithIdBeginingWithB(List<Spartan> spartans)
         {
-            throw new NotImplementedException();
+            return spartans.Where(x => x.SpartanId.StartsWith("B")).ToList();
         }
     }
 }
